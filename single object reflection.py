c8c8db823fc55a8377e389581a093e2b7ce054cc
Unknown <kfatseas@gmail.com>
2018-11-14 18:20:59 +0100

    added relative speed

--- single object reflection.py
+++ single object reflection.py
@@ -9,28 +9,41 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 x = np.arange(.1, 100, .1)
-y = 30
+y = 10
 z = 2 * np.sqrt(((x/2)**2) + (y**2))
 
-plt.subplot(2, 1, 1)
+plt.subplot(3, 1, 1)
 plt.plot(x,z)
-plt.xlabel('Real distance (m)')
-plt.ylabel('Reflection distance (m)')
-plt.title('Distance of the reflected object')
+plt.plot(x,x)
+plt.xlabel('Real range (m)')
+plt.ylabel('Reflection range (m)')
+plt.title('Range of the reflected object')
+plt.subplots_adjust(hspace = 1)
+plt.grid(True)
+
+dzdx = np.gradient(z, .1)
+
+plt.subplot(3, 1, 2)
+plt.plot(x,dzdx)
+plt.xlabel('Real range (m)')
+plt.ylabel('Reflection\'s speed (m)')
+plt.title('Relative speed of the reflected object')
+plt.subplots_adjust(hspace = 1)
 plt.grid(True)
 
 adj = x/2
 hyp = z/2
 angle = np.degrees(np.arccos(adj/hyp))
 
-plt.subplot(2, 1, 2)
+plt.subplot(3, 1, 3)
 plt.plot(x,angle)
 plt.xlabel('Real distance (m)')
 plt.ylabel('Reflection\'s angle of arrival (deg)')
 plt.title('AoA of the reflected object')
+plt.subplots_adjust(hspace = .5)
 plt.grid(True)
 
-plt.tight_layout()
+#plt.tight_layout()
 plt.show()
 
 """
